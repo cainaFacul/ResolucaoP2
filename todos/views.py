2198820d8d3d6e3d77ef3d5f153618b1b348baa0
caina <ra202221129@univassouras.edu.br>
2024-11-19 19:19:43 -0300

    questao 1

---
+++ todos/views.py
@@ -0,0 +1,13 @@
+from django.views.generic import ListView, CreateView
+from django.urls import reverse_lazy
+from models import Todo
+# Create your views here.
+
+
+class TodoListView(ListView):
+    model = Todo
+
+class TodoCreateView(CreateView):
+    model = Todo
+    fields = ['title', 'created_at', 'deadline', 'finished_at']
+    success_url = reverse_lazy('todo_list')
